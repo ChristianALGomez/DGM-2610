445e0b35fbe808dc19368536cb413da5a752e7a4
Christian Gomez <10721401@my.uvu.edu>
2019-02-07 16:27:23 -0700

    Reworking lvl

--- Prototype One/Assets/Scripts/FallingWallScript.cs
+++ Prototype One/Assets/Scripts/FallingWallScript.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class FallingWallScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
+    //player enters box collider
+    
+    //wall falls down
 }
