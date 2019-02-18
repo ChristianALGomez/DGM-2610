cbd8c0b4f9e4c8c6029c114e684cd34c61a36633
Christian Gomez <10721401@my.uvu.edu>
2019-02-18 16:45:56 -0700

    recording done in unity

---
+++ Prototype One/Assets/Scripts/Falling Bridge.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBridge : MonoBehaviour
+{
+   public AnimationClip Anim;
+
+   private void OnTriggerEnter(Collider other)
+   {
+      if (CompareTag("Player"))
+      {
+         
+      }
+   }
+}
