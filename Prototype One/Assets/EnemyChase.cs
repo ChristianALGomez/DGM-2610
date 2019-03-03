0174349587d85ae060a4c4b2c4ef8d4300aa9b67
Christian Gomez <10721401@my.uvu.edu>
2019-03-02 21:20:47 -0700

    redid chase code still needs work

---
+++ Prototype One/Assets/EnemyChase.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChase : MonoBehaviour
+{
+    public GameObject player;
+    public float MoveSP = 5;
+
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        transform.LookAt(target.transform);
+        transform.position += transform.forward * MoveSP * Time.deltaTime;
+    }*/
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == ("Player"))
+        {
+            transform.LookAt(player.transform);
+            transform.position += transform.forward * MoveSP * Time.deltaTime;
+        }
+    }
+}
