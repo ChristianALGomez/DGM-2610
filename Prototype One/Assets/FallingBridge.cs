2a6e1e6639171b58c9186f26d117a176a3a7245b
Christian Gomez <10721401@my.uvu.edu>
2019-03-13 14:24:49 -0600

    working on falling bridge

---
+++ Prototype One/Assets/FallingBridge.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBridge : MonoBehaviour
+{
+    private float xRotaion = 5;
+    //public GameObject target;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (CompareTag("Player"))
+        {
+            xRotaion += Input.GetAxis("Horizontal");
+            transform.eulerAngles = new Vector3(xRotaion, 0, 0);
+        }
+    }
+}
