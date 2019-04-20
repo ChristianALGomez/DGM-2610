ea22d0ffd99b61dbfc9f1870fa3f97e468249fd2
Christian Gomez <10721401@my.uvu.edu>
2019-04-20 16:57:25 -0600

    Enemy_Animations_Added

---
+++ Prototype One/Assets/Scripts/Enemy_Animation_Script.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Animation_Script : MonoBehaviour
+{
+    public bool move;
+    public bool attck;
+    public Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        move = true;
+        attck = false;
+    }
+
+    private void Update()
+    {
+        anim.SetBool("Attack",attck);
+        anim.SetBool("Move", move);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            move = false;
+            attck = true;
+        }
+        else
+        {
+            move = true;
+            attck = false;
+        }
+    }
+}
