46d01217d0dda5f758bbadccfcb619ee50a60805
Christian Gomez <10721401@my.uvu.edu>
2019-04-25 16:01:45 -0600

    Getting Enemy VFX working

---
+++ Prototype One/Assets/Scripts/Sprite_Enabler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprite_Enabler : MonoBehaviour
+{
+     
+    public GameObject partical1;
+    public GameObject partical2;
+
+
+    private void Start()
+    {
+        partical1.SetActive(false);
+        partical2.SetActive(false);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            partical1.SetActive(true);
+            partical2.SetActive(true);
+        }
+    }
+}
